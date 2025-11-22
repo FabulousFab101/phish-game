f921b9b80d98ca5d4c00745d3fd6f1a46a028ce1
TheGrandBraker <harrisontruong23@gmail.com>
2025-11-22 14:49:28 -0800

    cock and balls

--- script1.js
+++ script1.js
@@ -314,3 +314,9 @@ document.querySelectorAll('button.read').forEach(element => {
     clearEmailContents()
   })
 });
+document.querySelectorAll('button.start-button').forEach(element => {
+  element.addEventListener('click', (event) => {
+    offscreen = document.querySelector('div.off-screen')
+    offscreen.style.display = "none"
+  })
+});
